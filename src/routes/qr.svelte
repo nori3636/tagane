8f98afd49f28d73d3e814ea58a80f91461b3d7e7
kambe <nori.k3636@gmail.com>
2021-11-19 16:26:46 +0900

    QR機能を確認

--- src/routes/qr.svelte
+++ src/routes/qr.svelte
@@ -1,12 +1,60 @@
 <script lang="ts">
-	import { getAuth, signOut } from 'firebase/auth';
-	import { authStore } from '$lib/authStore';
-	import { goto } from '$app/navigation';
-	import { onDestroy } from 'svelte';
 	import Header from '../components/header.svelte';
+	import jsQR from 'jsqr';
+	import { onMount } from 'svelte';
+	onMount(() => {
+		let video = document.createElement('video');
+		let canvas = <HTMLCanvasElement>document.getElementById('canvas');
+		let ctx = canvas.getContext('2d');
+		let msg = document.getElementById('msg');
+
+		const userMedia = { video: { facingMode: 'environment' } };
+		navigator.mediaDevices.getUserMedia(userMedia).then((stream) => {
+			video.srcObject = stream;
+			video.setAttribute('playsinline', '');
+			video.play();
+			startTick();
+		});
+
+		function startTick() {
+			msg.innerText = 'Loading video...';
+			if (video.readyState === video.HAVE_ENOUGH_DATA) {
+				canvas.height = video.videoHeight;
+				canvas.width = video.videoWidth;
+				ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+				let img = ctx.getImageData(0, 0, canvas.width, canvas.height);
+				let code = jsQR(img.data, img.width, img.height, { inversionAttempts: 'dontInvert' });
+				if (code) {
+					drawRect(code.location); // Rect
+					msg.innerText = code.data; // Data
+				} else {
+					msg.innerText = 'Detecting QR-Code...';
+				}
+			}
+			setTimeout(startTick, 250);
+		}
+
+		function drawRect(location) {
+			drawLine(location.topLeftCorner, location.topRightCorner);
+			drawLine(location.topRightCorner, location.bottomRightCorner);
+			drawLine(location.bottomRightCorner, location.bottomLeftCorner);
+			drawLine(location.bottomLeftCorner, location.topLeftCorner);
+		}
+
+		function drawLine(begin, end) {
+			ctx.lineWidth = 4;
+			ctx.strokeStyle = '#FF3B58';
+			ctx.beginPath();
+			ctx.moveTo(begin.x, begin.y);
+			ctx.lineTo(end.x, end.y);
+			ctx.stroke();
+		}
+	});
 </script>
 
 <Header />
 <h1>QR</h1>
-
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div id="wrapper">
+	<div id="msg">Unable to access video stream.</div>
+	<canvas id="canvas" />
+</div>
