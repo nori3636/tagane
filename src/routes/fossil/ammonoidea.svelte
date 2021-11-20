5a1329decb73e1af1f54175571445878bc10488b
kambe <nori.k3636@gmail.com>
2021-11-21 02:03:17 +0900

    add image

---
+++ src/routes/fossil/ammonoidea.svelte
@@ -0,0 +1,40 @@
+<script>
+	import { authStore } from '$lib/authStore';
+	import { checkio, db } from '$lib/firebase';
+	import { doc, updateDoc } from 'firebase/firestore';
+	import Header from '$lib/components/header.svelte';
+	import { onMount } from 'svelte';
+
+	async function updatedb() {
+		try {
+			const Ref = doc(db, 'fossil', $authStore.userid);
+
+			// Set the "capital" field of the city 'DC'
+
+			await updateDoc(Ref, {
+				ammo: true
+			});
+			console.log('update');
+		} catch (e) {
+			console.log(e);
+		}
+	}
+	onMount(() => {
+		checkio();
+		updatedb();
+	});
+</script>
+
+<!-- svelte-ignore missing-declaration -->
+<Header />
+<h1>アンモナイト</h1>
+<img src="/ammo.jpg" alt="main" width="80%" />
+<p>
+	アンモナイト（分類名：アンモナイト亜綱、学名：subclassisAmmonoidea)
+	古生代シルル紀末期から中生代白亜紀末までのおよそ3億5000万年前後の間を、海洋に広く分布し繁栄した、頭足類の分類群の一つ。
+	多くの種が平らな巻き貝のような形をした殻を持っているのが特徴である。
+	アンモナイト亜綱は、オルドビス紀から生息するオウムガイ亜綱（英語版）の中から分化したものと考えられている。
+	以来、彼らは実に長くの時代を繁栄していたが、中生代の幕引きとなる白亜紀末のK-Pg境界を最後に地球上から姿を消した。
+	古生代と中生代の下位に当たる各年代を生きた種はそれぞれに示準化石とされており、地質学研究にとって極めて重要な生物群となっている。
+	出典：wikipedia
+</p>
