7bfe04a651fc108badf400bb7796dc7dfb949551
kaircx <kaircx@gmail.com>
2021-11-21 02:07:37 +0900

    fix

--- src/routes/qr.svelte
+++ src/routes/qr.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import Header from '$lib/components/header.svelte';
 	import Zukan from '$lib/components/zukan.svelte';
-	//import Qrreader from '../components/qrReader.svelte';
+	import Qrreader from '$lib/components/qrReader.svelte';
 </script>
 
 <Header />
-<!-- <Qrreader /> -->
+<Qrreader />
 <Zukan />
