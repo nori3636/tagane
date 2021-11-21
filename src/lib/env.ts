3685053719bafa7c12830c093ced937b9586bb7e
ssssota <tommysouta@gmail.com>
2021-11-21 21:48:02 +0900

    run format script

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,9 +1,9 @@
 export default {
-    apiKey: 'AIzaSyB1rH9ic_JCJ_UNM8yzkUbCZ_wj2O9WnMo',
-    authDomain: 'tagane.firebaseapp.com',
-    projectId: 'tagane',
-    storageBucket: 'tagane.appspot.com',
-    messagingSenderId: '890413177050',
-    appId: '1:890413177050:web:a57d805f1d92576f7e6de3',
-    measurementId: 'G-PQKM3EK5S8'
+	apiKey: 'AIzaSyB1rH9ic_JCJ_UNM8yzkUbCZ_wj2O9WnMo',
+	authDomain: 'tagane.firebaseapp.com',
+	projectId: 'tagane',
+	storageBucket: 'tagane.appspot.com',
+	messagingSenderId: '890413177050',
+	appId: '1:890413177050:web:a57d805f1d92576f7e6de3',
+	measurementId: 'G-PQKM3EK5S8'
 };
