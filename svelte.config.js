c31d66e7d9835f418f5233725dc0b64d0547e663
kambe <nori.k3636@gmail.com>
2021-11-21 14:20:41 +0900

    vercelアダプター追加

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import preprocess from 'svelte-preprocess';
+import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
+
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
@@ -9,6 +11,7 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
+		adapter: vercel(),
 		vite: {
 			ssr: {
 				external: ['firebase']
