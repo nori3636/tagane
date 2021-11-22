05924aa0f57ea32dbe2eabed8273feca9815a7cc
ssssota <tommysouta@gmail.com>
2021-11-22 17:00:49 +0900

    remove duplicate tag

--- src/lib/components/fossil.svelte
+++ src/lib/components/fossil.svelte
@@ -5,14 +5,12 @@
 	export let show: boolean;
 </script>
 
-{#if show}
-	<Column sm={2} md={4} lg={8} class="auma" noGutter>
+<Column sm={2} md={4} lg={8} class="auma" noGutter>
+	{#if show}
 		<img src={name + '.jpg'} alt="name" width="80%" />
 		<div><h4>{name}</h4></div>
-	</Column>
-{:else}
-	<Column sm={2} md={4} lg={8} class="auma" noGutter>
+	{:else}
 		<img src="non.jpg" alt="noimage" width="80%" />
 		<div class="margin-small center margin-middle">未発見</div>
-	</Column>
-{/if}
+	{/if}
+</Column>
