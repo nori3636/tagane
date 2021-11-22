085cc4019ebde66e1eccece93889e5b63c3eaa27
ssssota <tommysouta@gmail.com>
2021-11-23 02:07:22 +0900

    refactor user store

--- src/lib/stores/user.ts
+++ src/lib/stores/user.ts
@@ -1,20 +1,20 @@
 import { auth } from '$lib/firebase';
 import { onAuthStateChanged } from 'firebase/auth';
 import type { Readable } from 'svelte/store';
-import { writable } from 'svelte/store';
+import { readable } from 'svelte/store';
 
 export type UserInfo = {
 	id: string;
 	name: string;
 };
 
-export const user: Readable<UserInfo | undefined> = (() => {
-	const { subscribe, set } = writable<UserInfo | undefined>(undefined);
-
-	onAuthStateChanged(auth, (user) => {
-		if (user === null) set(undefined);
-		else set({ id: user.uid, name: user.displayName ?? '' });
-	});
-
-	return { subscribe };
-})();
+export const user: Readable<UserInfo | undefined> = readable<UserInfo | undefined>(
+	undefined,
+	(set) => {
+		const unsubscribe = onAuthStateChanged(auth, (user) => {
+			if (user === null) set(undefined);
+			else set({ id: user.uid, name: user.displayName ?? '' });
+		});
+		return () => unsubscribe();
+	}
+);
